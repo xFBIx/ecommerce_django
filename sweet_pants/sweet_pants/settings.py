29cdc1cf77ccbc84136480afe19a273b6c1a81fc
Rudradatt Dave <rudradattdave@gmail.com>
2024-06-14 18:40:06 +0530

    fixes

--- sweet_pants/sweet_pants/settings.py
+++ sweet_pants/sweet_pants/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY") or config.SECRET_KEY
+SECRET_KEY = config.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = int(os.environ.get("DEBUG", default=1)) or config.Debug
+DEBUG = config.Debug
 
 ALLOWED_HOSTS: List[str] = ["*"]
 CORS_ALLOW_ALL_ORIGINS = True
